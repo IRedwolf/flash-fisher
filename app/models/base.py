6fe4e6a441b206e7411c121cd0e600c197197eb2
tingfeng <2078449839@qq.com>
2019-04-01 09:45:28 +0800

    datetime

--- app/models/base.py
+++ app/models/base.py
@@ -5,6 +5,7 @@
 # @Site    : 
 # @File    : base.py
 # @Software: PyCharm
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
 from sqlalchemy import Column, Integer, SmallInteger
 from contextlib import contextmanager
@@ -26,8 +27,11 @@ db = SQLAlchemy()
 
 class Base(db.Model):
 	__abstract__ = True
-	# create_time = Column('create_time', Integer)
+	create_time = Column('create_time', Integer)
 	status = Column(SmallInteger, default=1)
+	
+	def __init__(self):
+		self.create_time = int(datetime.now().timestamp())
 	
 	def set_attrs(self, attrs_dict):
 		for key, value in attrs_dict.items():
--- app/web/gift.py
+++ app/web/gift.py
@@ -1,36 +1,35 @@
 from app.models.base import db
 from app.models.gift import Gift
 from . import web
-from flask import current_app,flash
-from flask_login import login_required,current_user
+from flask import current_app, flash, redirect, url_for
+from flask_login import login_required, current_user
+
 __author__ = '七月'
 
 
 @web.route('/my/gifts')
 @login_required
 def my_gifts():
-    return 'My Gifts'
+	return 'My Gifts'
 
 
 @web.route('/gifts/book/<isbn>')
 @login_required
 def save_to_gifts(isbn):
-    if current_user.can_save_to_list(isbn):
-        with db.auto_commit():
-            gift = Gift()
-            gift.isbn = isbn
-            gift.uid = current_user.id
-            current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
-            db.session.add(gift)
-      
-    else:
-        flash('这本书已添加至你的赠送清单或已存入你的心愿清单,请不要重复添加')
-
+	if current_user.can_save_to_list(isbn):
+		with db.auto_commit():
+			gift = Gift()
+			gift.isbn = isbn
+			gift.uid = current_user.id
+			current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
+			db.session.add(gift)
+	
+	else:
+		flash('这本书已添加至你的赠送清单或已存入你的心愿清单,请不要重复添加')
+	
+	return redirect(url_for('web.book_detail', isbn=isbn))
 
 
 @web.route('/gifts/<gid>/redraw')
 def redraw_from_gifts(gid):
-    pass
-
-
-
+	pass
